dce9ed43abc4840399039f0fd7027f61fe6abce8
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-22 16:27:31 +0200

    (partials) add header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,7 @@
-<div>footer</div>
+<footer>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Trains Station since 1918</span>
+        </div>
+    </nav>
+</footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,7 @@
-<div>Header <i class="fa-solid fa-home"></i></div>
+<header>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Trains Station</span>
+        </div>
+    </nav>
+</header>
