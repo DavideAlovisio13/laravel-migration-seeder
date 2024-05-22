4e89123c5c51d64d2c50b6b7b56fe5c6fd9ab48f
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-22 16:14:31 +0200

    (home.blade) print db in a table template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,40 @@
 
 @section('content')
 <main>
-    <h1>Home page</h1>
+    <ul>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">company</th>
+                    <th scope="col">departure_station</th>
+                    <th scope="col">arrival_station</th>
+                    <th scope="col">departure_time</th>
+                    <th scope="col">arrival_time</th>
+                    <th scope="col">train_code</th>
+                    <th scope="col">carriages</th>
+                    <th scope="col">on_time</th>
+                    <th scope="col">cancelled</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($trains as $train)
+                <tr>
+                    <th scope="row">{{$train->id}}</th>
+                    <td>{{$train->company}}</td>
+                    <td>{{$train->departure_station}}</td>
+                    <td>{{$train->arrival_station}}</td>
+                    <td>{{$train->departure_time}}</td>
+                    <td>{{$train->arrival_time}}</td>
+                    <td>{{$train->train_code}}</td>
+                    <td>{{$train->carriages}}</td>
+                    <td>{{$train->on_time}}</td>
+                    <td>{{$train->cancelled}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </ul>
 </main>
 
 @endsection
