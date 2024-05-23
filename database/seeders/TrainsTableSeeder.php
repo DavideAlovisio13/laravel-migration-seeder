2bd1cd4ea6b8cd70e16045e2cf9d61c3312acb58
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-23 14:35:26 +0200

    generate a folder in App for storage main functions

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -16,21 +16,26 @@ class TrainsTableSeeder extends Seeder
     public function run()
     {
         // variabile che attinge i dati dal file .json
-        $trains = json_decode(file_get_contents(__DIR__ . '/trains.json'), true);
+        // $trains = json_decode(file_get_contents(__DIR__ . '/trains.json'), true);
         // dd($trains);
-        foreach ($trains as $train) {
-            $new_train = new Train();
-            $new_train->id = $train['id'];
-            $new_train->company = $train['company'];
-            $new_train->departure_station = $train['departure_station'];
-            $new_train->arrival_station = $train['arrival_station'];
-            $new_train->departure_time = $train['departure_time'];
-            $new_train->arrival_time = $train['arrival_time'];
-            $new_train->train_code = $train['train_code'];
-            $new_train->carriages = $train['carriages'];
-            $new_train->on_time = $train['on_time'];
-            $new_train->cancelled = $train['cancelled'];
-            $new_train->save();
-        }
+        // foreach ($trains as $train) {
+        //     $new_train = new Train();
+        //     $new_train->id = $train['id'];
+        //     $new_train->company = $train['company'];
+        //     $new_train->departure_station = $train['departure_station'];
+        //     $new_train->arrival_station = $train['arrival_station'];
+        //     $new_train->departure_time = $train['departure_time'];
+        //     $new_train->arrival_time = $train['arrival_time'];
+        //     $new_train->train_code = $train['train_code'];
+        //     $new_train->carriages = $train['carriages'];
+        //     $new_train->on_time = $train['on_time'];
+        //     $new_train->cancelled = $train['cancelled'];
+        //     $new_train->save();
+        // }
+
+        // metodo di impostazione tramite file .csv
+
+        //creo una variabile che contiente il path del file 
+        $path = __DIR__ . '/trains.csv';
     }
 }
