b75c358c427564209f858423df3ea2d883f04351
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-23 15:14:30 +0200

    populate db

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
 
+//importo il mio file con le mie funzioni con un alias
+use App\Functions\Utilities as CsvReader;
+
 class TrainsTableSeeder extends Seeder
 {
     /**
@@ -37,5 +40,25 @@ class TrainsTableSeeder extends Seeder
 
         //creo una variabile che contiente il path del file 
         $path = __DIR__ . '/trains.csv';
+        // richiamo la mia funzione
+        $data = CsvReader::getCsvData($path);
+
+        foreach ($data as $index => $train) {
+            if($index !== 0) {  // salto la prima riga che contiene i nomi delle colonne
+                $new_train = new Train();
+                $new_train->id = $train[0];
+                $new_train->company = $train[1];
+                $new_train->departure_station = $train[2];
+                $new_train->arrival_station = $train[3];
+                $new_train->departure_time = $train[4];
+                $new_train->arrival_time = $train[5];
+                $new_train->train_code = $train[6];
+                $new_train->carriages = $train[7];
+                $new_train->on_time = $train[8];
+                $new_train->cancelled = $train[9];
+                $new_train->save();
+            }
+        }
+
     }
 }
